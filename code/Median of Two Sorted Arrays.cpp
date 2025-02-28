f54f060fe1036f3b60e13011ddd15fda5da10d58
Nikhil N Singh <nsingh32297@gmail.com>
2025-02-28 15:15:30 +0530

    Create Median of Two Sorted Arrays.cpp

---
+++ code/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    const int n1 = nums1.size();
+    const int n2 = nums2.size();
+    if (n1 > n2)
+      return findMedianSortedArrays(nums2, nums1);
+
+    int l = 0;
+    int r = n1;
+…};
